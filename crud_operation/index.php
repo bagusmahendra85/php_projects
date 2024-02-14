a5e63727d34588ac0d6186b46e4ae340d6da1960
bagusmahendra85 <bagus.putramahendra@gmail.com>
2024-02-14 23:43:10 +0800

    chore: working on some front end, and add login page

--- crud_operation/index.php
+++ crud_operation/index.php
@@ -10,87 +10,99 @@ require "./functions/function.php";
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Halaman Admin</title>
+    <!-- my css -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/9ed35d004b.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    <h1>Data Penduduk</h1>
-    <div class="container-fluid">
-      <button type="button" class="btn btn-success">Tambah Data</button>
-    </div>
-    <div class="table-responsive">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">No</th>
-            <th scope="col">Foto</th>
-            <th scope="col">Aksi</th>
-            <th scope="col">NIK</th>
-            <th scope="col">Nomor KK</th>
-            <th scope="col">Nama</th>
-            <th scope="col">Banjar</th>
-            <th scope="col">Tempat Lahir</th>
-            <th scope="col">Tanggal Lahir</th>
-            <th scope="col">Jenis Kelamin</th>
-            <th scope="col">Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <!-- Index Field -->
-            <th scope="row">1</th>
-            <!-- Profile Photo Field -->
-            <td><img src="" alt=""></td>
-            <!-- Actions Field -->
-            <td>
-              <button type="button" class="btn btn-outline-primary btn-sm">Edit</button>
-              <button type="button" class="btn btn-outline-danger btn-sm">Hapus</button>
-            </td>
-            <!-- NIK Field -->
-            <td>5103020805960008</td>
-            <!-- Nomor KK Field -->
-            <td>5103021415210012</td>
-            <!-- Nama Field -->
-            <td>I Gst. Ngr. Bagus Putra Mahendra Yasa</td>
-            <!-- Banjar Field -->
-            <td>Alangkajeng</td>
-            <!-- Tempat Lahir Field -->
-            <td>Mengwi</td>
-            <!-- Tgl Lahir Field -->
-            <td>1996-05-08</td>
-            <!-- Jenis Kelamin Field -->
-            <td>Laki-Laki</td>
-            <!-- Email Field -->
-            <td>ngurahmahendra@mengwi-badung.desa.id</td>
-          </tr>
-          <tr>
-            <!-- Index Field -->
-            <th scope="row">2</th>
-            <!-- Profile Photo Field -->
-            <td><img src="" alt=""></td>
-            <!-- Actions Field -->
-            <td>
-              <button type="button" class="btn btn-outline-primary btn-sm">Edit</button>
-              <button type="button" class="btn btn-outline-danger btn-sm">Hapus</button>
-            </td>
-            <!-- NIK Field -->
-            <td>5103025805960002</td>
-            <!-- Nomor KK Field -->
-            <td>5103021415210012</td>
-            <!-- Nama Field -->
-            <td>Ni Komang Urmila Dewi</td>
-            <!-- Banjar Field -->
-            <td>Alangkajeng</td>
-            <!-- Tempat Lahir Field -->
-            <td>Denpasar</td>
-            <!-- Tgl Lahir Field -->
-            <td>1996-05-18</td>
-            <!-- Jenis Kelamin Field -->
-            <td>Perempuan</td>
-            <!-- Email Field -->
-            <td>urmiladewi18@gmail.com</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="container">
+      <h1>Data Penduduk</h1>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <button type="button" class="btn btn-success "><i class="fa-regular fa-square-plus"></i> Tambah Data</button>
+          <form class="d-flex" role="search" action="" method="post" id="search">
+            <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit" name="search">Search</button>
+          </form>
+        </div>
+      </nav>
+      <div class="table-responsive">
+        <table class="table table-bordered">
+          <thead class="table-secondary">
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Foto</th>
+              <th scope="col">Aksi</th>
+              <th scope="col">NIK</th>
+              <th scope="col">Nomor KK</th>
+              <th scope="col">Nama</th>
+              <th scope="col">Banjar</th>
+              <th scope="col">Tempat Lahir</th>
+              <th scope="col">Tanggal Lahir</th>
+              <th scope="col">Jenis Kelamin</th>
+              <th scope="col">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <!-- Index Field -->
+              <th scope="row">1</th>
+              <!-- Profile Photo Field -->
+              <td><img src="" alt=""></td>
+              <!-- Actions Field -->
+              <td>
+                <button type="button" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button type="button" class="btn btn-outline-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
+              </td>
+              <!-- NIK Field -->
+              <td><a href="#">5103020805960008</a></td>
+              <!-- Nomor KK Field -->
+              <td><a href="#">5103021415210012</a></td>
+              <!-- Nama Field -->
+              <td>I Gst. Ngr. Bagus Putra Mahendra Yasa</td>
+              <!-- Banjar Field -->
+              <td>Alangkajeng</td>
+              <!-- Tempat Lahir Field -->
+              <td>Mengwi</td>
+              <!-- Tgl Lahir Field -->
+              <td>1996-05-08</td>
+              <!-- Jenis Kelamin Field -->
+              <td>Laki-Laki</td>
+              <!-- Email Field -->
+              <td>ngurahmahendra@mengwi-badung.desa.id</td>
+            </tr>
+            <tr>
+              <!-- Index Field -->
+              <th scope="row">2</th>
+              <!-- Profile Photo Field -->
+              <td><img src="" alt=""></td>
+              <!-- Actions Field -->
+              <td>
+                <button type="button" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button type="button" class="btn btn-outline-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
+              </td>
+              <!-- NIK Field -->
+              <td><a href="#">5103025805960002</a></td>
+              <!-- Nomor KK Field -->
+              <td><a href="#">5103021415210012</a></td>
+              <!-- Nama Field -->
+              <td>Ni Komang Urmila Dewi</td>
+              <!-- Banjar Field -->
+              <td>Alangkajeng</td>
+              <!-- Tempat Lahir Field -->
+              <td>Denpasar</td>
+              <!-- Tgl Lahir Field -->
+              <td>1996-05-18</td>
+              <!-- Jenis Kelamin Field -->
+              <td>Perempuan</td>
+              <!-- Email Field -->
+              <td>urmiladewi18@gmail.com</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
---
+++ crud_operation/login/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/9ed35d004b.js" crossorigin="anonymous"></script>
+  <style>
+
+      body {
+        background-image: radial-gradient( circle farthest-corner at 10% 20%,  rgba(0,152,155,1) 0.1%, rgba(0,94,120,1) 94.2% );
+      }
+
+      main {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      form {
+        /* glassmorphism */
+        background: rgba(255, 255, 255, 0.2);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(5px);
+        -webkit-backdrop-filter: blur(5px);
+        border: 1px solid rgba(255, 255, 255, 0.3);
+        /* glassmorphism end */
+        width: 20em;
+        border-radius: 10px;
+        margin-bottom: 200px;
+        color: #fff;
+      }
+
+      form img {
+        max-width: 80px;
+      }
+
+  </style>
+</head>
+<body>
+  <main class="container-fluid">
+    
+    <form class="p-4">
+      <!-- logo -->
+      <div class="container d-flex align-item-center justify-content-center mb-2">
+        <img src="../assets/pic/logo-desa-mengwi-sm-300px.png" alt="logo desa mengwi">
+      </div>
+      <!-- username -->
+      <div class="mb-3">
+        <label for="username" class="form-label" autofocus autocomplete="off" >Username</label>
+        <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">  
+      </div>
+      <!-- password -->
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+      </div>
+      <!-- submit button -->
+      <button type="submit" name="submit" class="btn btn-primary">Login</button>
+    </form>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
