932e66fa19c58d2fa1006281a1751ffdb042e140
bagusmahendra85 <bagus.putramahendra@gmail.com>
2024-02-17 14:04:36 +0800

    fix: refactor delete feature

--- crud_operation/public/drop.php
+++ crud_operation/public/drop.php
@@ -2,6 +2,20 @@
 require '../include/function.php';
 
 $id = $_GET["id"];
-drop($id);
+if (drop($id) > 0) {
+  echo "
+  <script>
+    alert('Data Berhasil Dihapus');
+    document.location.href = 'index.php';
+  </script>
+  ";
+} else {
+  echo "
+  <script>
+    alert('Data Gagal Dihapus');
+    document.location.href = 'index.php';
+  </script>
+  ";
+}
 
 ?>
