e4747296ddfd7e9f9c374d7a4ce3ebdcf5136cff
bagusmahendra85 <bagus.putramahendra@gmail.com>
2024-02-17 21:28:05 +0800

    update: update database dump and required files

--- crud_operation/databases_dump/database.dump.sql
+++ crud_operation/databases_dump/database.dump.sql
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `crud_data_banjar`
+--
+
+DROP TABLE IF EXISTS `crud_data_banjar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `crud_data_banjar` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama_banjar` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `crud_data_banjar`
+--
+
+LOCK TABLES `crud_data_banjar` WRITE;
+/*!40000 ALTER TABLE `crud_data_banjar` DISABLE KEYS */;
+INSERT INTO `crud_data_banjar` VALUES (1,'Batu'),(2,'Gambang'),(3,'Pande'),(4,'Munggu'),(5,'Pandean'),(6,'Serangan'),(7,'Peregae'),(8,'Lebah Pangkung'),(9,'Pengiasan'),(10,'Alangkajeng'),(11,'Delod Bale Agung');
+/*!40000 ALTER TABLE `crud_data_banjar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `crud_data_penduduk`
 --
 
@@ -39,7 +63,7 @@ CREATE TABLE `crud_data_penduduk` (
   KEY `jenis_kelamin` (`jenis_kelamin`),
   CONSTRAINT `crud_data_penduduk_ibfk_1` FOREIGN KEY (`banjar`) REFERENCES `ref_banjar` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `crud_data_penduduk_ibfk_2` FOREIGN KEY (`jenis_kelamin`) REFERENCES `ref_gender` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,8 +72,32 @@ CREATE TABLE `crud_data_penduduk` (
 
 LOCK TABLES `crud_data_penduduk` WRITE;
 /*!40000 ALTER TABLE `crud_data_penduduk` DISABLE KEYS */;
-INSERT INTO `crud_data_penduduk` VALUES (1,'5103020805960008','5103021415210012','I Gst Ngr Bagus Putra Mahendra Yasa',10,'Mengwi','1996-05-08',1,'ngurahmahendra@mengwi-badung.desa.id','mahendra.jpg'),(2,'5103025805960002','5103021415210012','Ni Komang Urmila Dewi',10,'Denpasar','1996-05-18',2,'urmiladewi18@gmail.com','urmila.jpg'),(3,'5103020301230004','5103021415210012','I Gusti Ayu Manik Mandala Putri',10,'Tabanan','2023-01-09',2,'manik.mandala@gmail.com','mandala.jpg');
+INSERT INTO `crud_data_penduduk` VALUES (1,'5103020805960008','5103021415210012','I Gst Ngr Bagus Putra Mahendra Yasa',10,'Mengwi','1996-05-08',1,'ngurahmahendra@mengwi-badung.desa.id','65d08ec851bcc.webp'),(2,'5103025805960002','5103021415210012','Ni Komang Urmila Dewi',10,'Denpasar','1996-05-18',2,'urmiladewi18@gmail.com','65d08ece4dc85.webp'),(4,'5103020901230004','5103021415210012','I Gusti Ayu Manik Mandala Putri',10,'Tabanan','2023-01-09',2,'manik.mandalaputri@gmail.com','65d08eda23cf4.webp'),(5,'5103021508820004','5103020711120014','I Putu Gede Astrawan',4,'Mengwi','1982-08-15',1,'astrawan.putu@mengwi-badung.desa.id','65d08ed447962.webp'),(6,'5103020506890002','5103021106180009','Ida Bagus Putu Ari Suryawan',2,'Badung','1989-06-05',1,'gustu_ary@gmail.com','65d08ee079f27.webp'),(7,'5103021011960002','5103020711120014','Ni Putu Nita Ningsih',4,'Mengwi','1996-11-10',2,'thanitha96@gmail.com','65d0b1033bf30.webp');
 /*!40000 ALTER TABLE `crud_data_penduduk` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `crud_meta_jenis_kelamin`
+--
+
+DROP TABLE IF EXISTS `crud_meta_jenis_kelamin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `crud_meta_jenis_kelamin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `crud_meta_jenis_kelamin`
+--
+
+LOCK TABLES `crud_meta_jenis_kelamin` WRITE;
+/*!40000 ALTER TABLE `crud_meta_jenis_kelamin` DISABLE KEYS */;
+INSERT INTO `crud_meta_jenis_kelamin` VALUES (1,'Laki-Laki'),(2,'Perempuan');
+/*!40000 ALTER TABLE `crud_meta_jenis_kelamin` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -109,4 +157,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-14 23:45:08
+-- Dump completed on 2024-02-17 21:25:37
