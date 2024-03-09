c284df2ebfd45eb28b39ec0ff8f7d91c03f600c9
bagusmahendra85 <bagus.putramahendra@gmail.com>
2024-03-09 14:27:33 +0800

    chore: add controller

--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -1,17 +1,24 @@
 <?php 
 
 class App {
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
+
   public function __construct()
   {
     $url = $this -> parseURL();
-    var_dump($url);
+    
   }
 
   public function parseURL()
   {
     if (isset($_GET["url"])) {
+      // remove slash "/" from the end of URL
       $url = rtrim($_GET["url"], '/');
+      // sanitize url from malicious input
       $url = filter_var($url, FILTER_SANITIZE_URL);
+      // map url into array
       $url = explode('/', $url);
       return $url;
     }
